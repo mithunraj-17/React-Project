ce163c1ee84cec2e9e5656d4e0374eed6ff398de
mithunraj-17 <727723eucs120@skcet.ac.in>
2024-11-14 09:52:41 +0530

    Create CartContext.js

---
+++ context/CartContext.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from 'react';
+
+// Create the CartContext
+export const CartContext = createContext();
+
+// Create a CartProvider component
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems((prevItems) => [...prevItems, item]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
